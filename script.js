2893f49e438812ba06129f0ff39c60501763559e
bianca <168255221+kvjeon@users.noreply.github.com>
2024-05-03 20:27:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('heart1').addEventListener('click', function() {
-  alert('Happy Monthsary, laloves ko!');
+  alert('Happy 2nd Monthsary, laloves ko!');
 });
 
 document.getElementById('heart2').addEventListener('click', function() {
@@ -15,15 +15,15 @@ document.getElementById('heart4').addEventListener('click', function() {
 });
 
 document.getElementById('picture1').addEventListener('click', function() {
-  alert('Tandaan mo gid na di ta gid ka ya pagbayaan');
+  alert('Tandaan mo gid na di ta gid ka ya pagbayaan mwaaaa');
 });
 
 document.getElementById('picture2').addEventListener('click', function() {
-  alert('soaper swerte ko kay ikaw migo ko mwaaa');
+  alert('Permi ta ka na pray before ko magtulog nga di mag bag o treatment mo sakon : (');
 });
 
 document.getElementById('picture3').addEventListener('click', function() {
-  alert('Biskan naaway ta ka, thank you kay kay wala mo ko dyapon nabayaan');
+  alert('Biskan naaway ta ka, thank you kay wala mo ko dyapon nabayaan hehe sorry kung dungol ko : (');
 });
 
 document.getElementById('picture4').addEventListener('click', function() {
