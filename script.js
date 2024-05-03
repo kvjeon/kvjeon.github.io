8dfc6256cf639a6effcea5f6226b7cb8d918bbcf
bianca <168255221+kvjeon@users.noreply.github.com>
2024-05-03 19:37:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('heart1').addEventListener('click', function() {
+  alert('Happy Monthsary, laloves ko!');
+});
+
+document.getElementById('heart2').addEventListener('click', function() {
+  alert('Super duper thankful gid ko kay nakilala ta ka!');
+});
+
+document.getElementById('heart3').addEventListener('click', function() {
+  alert('Thank you for making me smile, love.');
+});
+
+document.getElementById('heart4').addEventListener('click', function() {
+  alert('Iloveyou so much poooo!');
+});
+
+document.getElementById('picture1').addEventListener('click', function() {
+  alert('Tandaan mo gid na di ta gid ka ya pagbayaan');
+});
+
+document.getElementById('picture2').addEventListener('click', function() {
+  alert('soaper swerte ko kay ikaw migo ko mwaaa');
+});
+
+document.getElementById('picture3').addEventListener('click', function() {
+  alert('Biskan naaway ta ka, thank you kay kay wala mo ko dyapon nabayaan');
+});
+
+document.getElementById('picture4').addEventListener('click', function() {
+  alert('Imissyou so much : (');
+});
